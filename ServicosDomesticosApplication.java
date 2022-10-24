362f7fbf25e95e42f4bf2149843bdd361dd465d6
faxinasTcc <116560779+faxinasTcc@users.noreply.github.com>
2022-10-24 08:29:09 -0300

    Add files via upload

---
+++ ServicosDomesticosApplication.java
@@ -0,0 +1,13 @@
+package com.itb.tcc.servicosdomesticos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicosDomesticosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicosDomesticosApplication.class, args);
+	}
+
+}
